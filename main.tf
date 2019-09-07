4066b740914718f20b682e6668db41b7d9fa3686
nc <mixtapeking@mail.ru>
2019-09-07 11:39:17 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-0c64dd618a49aeee8"
+  # ami = "ami-0c64dd618a49aeee8"
+  ami = "ami-b374d5a5"
   instance_type = "t2.micro"
 }
