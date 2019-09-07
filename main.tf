a3b31c3b89768b3c58be8f1adbcce931d70c7194
nc <mixtapeking@mail.ru>
2019-09-07 11:58:46 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   # ami = "ami-0c64dd618a49aeee8"
-  ami = "ami-05c1fa8df71875112"
+  ami           = "ami-05c1fa8df71875112"
   instance_type = "t2.micro"
+  count         = 2
+  tags          = { Name = "slave" }
+}
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example.id
 }
