4ea0f9bc3ddf7955ab58d6f9759f3aaa1324b445
nc <mixtapeking@mail.ru>
2019-09-07 12:15:07 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,13 +4,18 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  # ami = "ami-0c64dd618a49aeee8"
   ami           = "ami-05c1fa8df71875112"
   instance_type = "t2.micro"
-  tags          = { Name = "slave" }
+  tags          = { Name = "ubuntu-slave" }
 }
 
-resource "aws_eip" "ip" {
-    vpc = true
-    instance = aws_instance.example.id
+resource "aws_instance" "example" {
+  ami           = "ami-0c64dd618a49aeee8"
+  instance_type = "t2.micro"
+  tags          = { Name = "amazon-slave" }
 }
+
+# resource "aws_eip" "ip" {
+#     vpc = true
+#     instance = aws_instance.example.id
+# }
