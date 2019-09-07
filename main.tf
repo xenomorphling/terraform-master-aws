4655e926340dd387d35885aec9ffe6ad03a6e96d
nc <mixtapeking@mail.ru>
2019-09-07 13:32:02 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "ubuntu" {
   tags          = { Name = "ubuntu-slave" }
 
   provisioner "local-exec" {
-    command = "echo ${aws_instance.tags.Name, aws_instance.ubuntu.public_ip} > ip_address.txt"
+    command = "echo ${aws_instance.tags.Name + aws_instance.ubuntu.public_ip} > ip_address.txt"
   }
 }
 
