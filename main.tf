f1f15582c145ee6f0c9662336c3c2a9787b12f82
nc <mixtapeking@mail.ru>
2019-09-11 14:34:20 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,6 +23,10 @@ resource "aws_instance" "amazon_linux" {
   }
 }
 
+output "ip" {
+  value = aws_eip.ip.public_ip
+}
+
 # resource "aws_eip" "ip" {
 #     vpc = true
 #     instance = aws_instance.example.id
