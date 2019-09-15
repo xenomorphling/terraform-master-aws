78de00d88a729b73be7df4552868107babe80b8d
nc <mixtapeking@mail.ru>
2019-09-15 18:23:10 +0300

    added servers suse

--- main.tf
+++ main.tf
@@ -3,45 +3,45 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = var.amis-ubuntu[var.region]
-  instance_type = "t2.micro"
-  tags          = { Name = "ubuntu-slave" }
-  key_name      = "ernestlawrence_ohio_aws"
-
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.ubuntu.public_ip} > ip_address.txt"
-  }
-
-  provisioner "file" {
-    source      = "sshd_config"
-    destination = "/etc/ssh/sshd_config"
-  }
-}
-
-resource "aws_instance" "suse" {
-  ami           = var.amis-suse[var.region]
-  instance_type = "t2.micro"
-  tags          = { Name = "suse-slave" }
-  count         = 5
-  key_name      = "ernestlawrence_ohio_aws"
-
-  provisioner "file" {
-    source      = "sshd_config"
-    destination = "/etc/ssh/sshd_config"
-  }
-
-  //  provisioner "local-exec" {
-//    command = "echo ${aws_instance.suse[count.index]} >> ip_address.txt"
+//resource "aws_instance" "ubuntu" {
+//  ami           = var.amis-ubuntu[var.region]
+//  instance_type = "t2.micro"
+//  tags          = { Name = "ubuntu-slave" }
+//  key_name      = "ernestlawrence_ohio_aws"
+//
+//  provisioner "local-exec" {
+//    command = "echo ${aws_instance.ubuntu.public_ip} > ip_address.txt"
 //  }
-}
+//
+//  provisioner "file" {
+//    source      = "sshd_config"
+//    destination = "/etc/ssh/sshd_config"
+//  }
+//}
+
+//resource "aws_instance" "suse" {
+//  ami           = var.amis-suse[var.region]
+//  instance_type = "t2.micro"
+//  tags          = { Name = "suse-slave" }
+//  count         = 5
+//  key_name      = "ernestlawrence_ohio_aws"
+//
+//  provisioner "file" {
+//    source      = "sshd_config"
+//    destination = "/etc/ssh/sshd_config"
+//  }
+//
+//  //  provisioner "local-exec" {
+////    command = "echo ${aws_instance.suse[count.index]} >> ip_address.txt"
+////  }
+//}
 
 resource "aws_instance" "amazon_linux" {
   ami           = var.amis-linux[var.region]
   instance_type = "t2.micro"
   tags          = { Name = "amazon-slave" }
   key_name      = "ernestlawrence_ohio_aws"
-
+  count = 5
 
   provisioner "local-exec" {
 //    command = "echo ${aws_instance.amazon_linux.tags.Name}${aws_instance.ubuntu.public_ip} > ip_address.txt"
