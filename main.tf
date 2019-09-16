7d6ff0db548ad5e5cc0ac215b4340a47674c17a7
nc <mixtapeking@mail.ru>
2019-09-16 08:34:30 +0300

    added servers amazon

--- main.tf
+++ main.tf
@@ -3,21 +3,22 @@ provider "aws" {
   region = var.region
 }
 
-//resource "aws_instance" "ubuntu" {
-//  ami           = var.amis-ubuntu[var.region]
-//  instance_type = "t2.micro"
-//  tags          = { Name = "ubuntu-slave" }
-//  key_name      = "ernestlawrence_ohio_aws"
-//
-//  provisioner "local-exec" {
-//    command = "echo ${aws_instance.ubuntu.public_ip} > ip_address.txt"
-//  }
-//
-//  provisioner "file" {
-//    source      = "sshd_config"
-//    destination = "/etc/ssh/sshd_config"
-//  }
-//}
+resource "aws_instance" "ubuntu" {
+  ami           = var.amis-ubuntu[var.region]
+  instance_type = "t2.micro"
+  tags          = { Name = "ubuntu-slave" }
+  key_name      = "ernestlawrence_ohio_aws"
+  count         = 2
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.ubuntu.public_ip} > ip_address.txt"
+  }
+
+  provisioner "file" {
+    source      = "sshd_config"
+    destination = "/etc/ssh/sshd_config"
+  }
+}
 
 //resource "aws_instance" "suse" {
 //  ami           = var.amis-suse[var.region]
@@ -36,12 +37,12 @@ provider "aws" {
 ////  }
 //}
 
-resource "aws_instance" "amazon_linux" {
-  ami           = var.amis-linux[var.region]
-  instance_type = "t2.micro"
-  tags          = { Name = "amazon-slave[count.index]" }
-  key_name      = "ernestlawrence_ohio_aws"
-  count = 5
+//resource "aws_instance" "amazon_linux" {
+//  ami           = var.amis-linux[var.region]
+//  instance_type = "t2.micro"
+//  tags          = { Name = "amazon-slave[count.index]" }
+//  key_name      = "ernestlawrence_ohio_aws"
+//  count = 5
 
 //  provisioner "local-exec" {
 ////    command = "echo ${aws_instance.amazon_linux.tags.Name}${aws_instance.ubuntu.public_ip} > ip_address.txt"
